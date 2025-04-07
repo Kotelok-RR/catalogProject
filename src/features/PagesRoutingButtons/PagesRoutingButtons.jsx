3dc9620432d998d4958e4ac835eda1047a5e7c6e
kotelok <petrov16052006@gmail.com>
2025-04-07 20:48:03 +0300

    Fix: active routing-button style now depends on user address

--- src/features/PagesRoutingButtons/PagesRoutingButtons.jsx
+++ src/features/PagesRoutingButtons/PagesRoutingButtons.jsx
@@ -4,22 +4,32 @@ import Button from '@components/Button/Button.jsx'
 
 import styles from './PagesRoutingButtons.module.css'
 
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const PagesRoutingButtons = () => {
     const navigate = useNavigate();
+    const location = useLocation();
+    const pathname = location.pathname;
 
     return (
         <div className = {styles['pages-routing__container']}>
                 <Button 
                     buttonType = {'button'}
-                    className = {classNames(styles['button-routing'], styles['button-routing_type-catalog'], styles['button-routing-is-active'])}
-                    onClick={() => navigate('/catalog')}
+                    onClick = {() => navigate('/catalog')}
+                    className={classNames(
+                        styles['button-routing'],
+                        styles['button-routing_type-catalog'],
+                        pathname === '/catalog' && styles['button-routing-is-active']
+                    )}
                     buttonContent = {'Каталог'}
                 />
                 <Button 
                     buttonType = {'button'}
-                    className = {classNames(styles['button-routing'], styles['button-routing_type-brands'])}
+                    className={classNames(
+                        styles['button-routing'],
+                        styles['button-routing_type-brands'],
+                        pathname === '/brands' && styles['button-routing-is-active']
+                    )}
                     onClick = {() => navigate('/brands')}
                     buttonContent = {'Бренды'}
                 />
